4ab5f6d13de4ad294098b6760983d5980503ed7a
Anna454545 <zhulidova.a@gmail.com>
2023-03-20 14:01:02 +0500

    insert student

---
+++ addStudent.php
@@ -0,0 +1,17 @@
+<?php
+
+$fname = stripcslashes(htmlspecialchars(trim($_POST['fname'])));
+$lname = stripcslashes(htmlspecialchars(trim($_POST['lname'])));
+$age = stripcslashes(htmlspecialchars(trim($_POST['age'])));
+require("config.php");
+$connect = new mysqli(HOST, USER, PASS, DB);
+        $connect->set_charset ("UTF8");
+$sql = "INSERT INTO `students`(`fname`,`lname`,`age`,`group_id`)
+VALUES ($fname, $lname,$age,1)";
+$result = $connect->query ($sql);
+if ($result) {
+    echo "Данные записаны";
+} else {
+    echo "Ошибка записи";
+}
+
--- index.php
+++ index.php
@@ -9,12 +9,16 @@
 </head>
 <body>
     <h1> Список студентов </h1>
+<form action="addStudent.php" method="POST">
+    <input type="text" name="fname" placeholder="Введите имя"required> <br>
+    <input type="text" name="lname" placeholder="Введите фамилию"required> <br>
+    <input type="number" name="age" placeholder="Введите возраст"required> <br>
+    <input type="submit" value="Добавить"><br>
+>
     <?php 
-        define("HOST", "localhost");
-        define("USER","root");
-        define("PASS","root");
-        define ("DB", "mydb");
+        require("config.php");
         $connect = new mysqli(HOST, USER, PASS, DB);
+        $connect->set_charset ("UTF8");
         $sql = "SELECT * FROM `students` JOIN `groups` ON `groups`.group_id = students.group_id ORDER BY age";
         
         $result = $connect ->query ($sql);
@@ -24,6 +28,7 @@
             $row[lname] $row[fname] $row[age] $row[name]
                     </p>";
         }
+        $connect->close();
         ?>
         
 
